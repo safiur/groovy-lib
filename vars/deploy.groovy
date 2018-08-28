7772fcfa53c8b2532bd3e65493cdfffb391adb01
root <root@localhost.localdomain>
2018-08-28 17:32:48 -0400

    hhsfhds

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,4 @@
 def call(def server, def port) {
-    sshagent(['RemoteCredentials']) {
         sh "scp target/*.jar root@${server}:/usr/local/tomcat/webapps/ROOT/"
         sh "/usr/local/tomcat/bin/startup.sh"
     }
