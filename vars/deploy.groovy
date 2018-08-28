461f1cc077f81fc1f0b7fa0e54243666bc95ea7d
root <root@localhost.localdomain>
2018-08-28 18:41:20 -0400

    hhsfhds

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,3 @@
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
-
 import static groovyx.net.http.ContentType.JSON
 import static groovyx.net.http.Method.POST
 import groovyx.net.http.HTTPBuilder
