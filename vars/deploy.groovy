5d32991a8af75734e938725145384ac5e972af70
root <root@localhost.localdomain>
2018-08-28 17:34:52 -0400

    hhsfhds

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,4 @@
 def call(def server, def port) {
         sh "scp target/*.jar root@${server}:/usr/local/tomcat/webapps/ROOT/"
-        sh "/usr/local/tomcat/bin/startup.sh"
-    }
+        sh "/usr/local/tomcat/bin/startup.sh" 
 }
